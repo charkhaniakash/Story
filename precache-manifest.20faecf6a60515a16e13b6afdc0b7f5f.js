103f44fda9245ae815cc7d223c24eeb6ca6037f4
Akash <charkhaniakash@gmail.com>
2022-06-17 23:07:44 +0530

    Updates

---
+++ precache-manifest.20faecf6a60515a16e13b6afdc0b7f5f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccdf50095b8bf76fb31da479210f546c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b959ee2a6658c0d0299",
+    "url": "/static/js/2.235e61d4.chunk.js"
+  },
+  {
+    "revision": "896d71639935452549ec",
+    "url": "/static/js/main.ffb22c25.chunk.js"
+  },
+  {
+    "revision": "583e51a7281672351041",
+    "url": "/static/js/runtime-main.38f647cd.js"
+  }
+]);
